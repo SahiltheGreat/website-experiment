e52feede0f8f8ba1fce678dde6b04bddffa40d58
SahiltheGreat <smbstar1@gmail.com>
2019-10-15 08:05:01 +0530

    jspart

--- js/quiz2.js
+++ js/quiz2.js
@@ -1,8 +1,8 @@
 answer=["clock","short","Yesterday, Today, and Tomorrow",
 		"A bottle","A towel",
 		"Because the batteries died","C",
-		"One was bald","A table","The letter M"];
-divNames=["hidden1","hidden2","hidden3","hidden4","hidden5","hidden6","hidden7","hidden8","hidden9","hidden10"];
+		"One was bald","A table","The letter M","A Candle"];
+divNames=["hidden1","hidden2","hidden3","hidden4","hidden5","hidden6","hidden7","hidden8","hidden9","hidden10","hidden11"];
 function showAnswer(i)
 {
 	var ansIndex=i-1
